520870bd6903add410309c5c0d7dc2e3cb74512e
Man Chong Leong <ml69@rice.edu>
2018-03-30 17:35:58 -0500

    script for testing

---
+++ testing_setUpParallel.R
@@ -0,0 +1,38 @@
+library(parallel)
+library(h2o)
+library(foreign)
+library(maptools)
+library(maps)
+library(spdep)
+library(sp)
+library(rgdal)
+library(plyr)
+numCores <- detectCores()
+clust <- makeCluster(numCores)
+h2o.init(nthreads = 4)
+a<-Sys.time()
+cbgs <- readOGR(dsn = "/Users/manchongleong/Desktop/Crate/Projects/NIH/Env_Mixtures/Racial_isolation_index_calculation/Data",
+                   layer = "NC_surrounding_bg_carto_2010")
+# Import dbf files.
+dbfBgs <- read.dbf("/Users/manchongleong/Desktop/Crate/Projects/NIH/Env_Mixtures/Racial_isolation_index_calculation/Data/cen2010_NC_surrounding_BG_RI.dbf")
+# Create a variable with the same name as cbgs.
+dbfBgs$GEOID10 <- dbfBgs$stfid10_bg
+# Merge two dataframe.
+mergeCbgs <- merge(cbgs, dbfBgs, by = "GEOID10")
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # Step 1.2 Create adjacency matrix # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # #
+
+
+summary(mergeCbgs)
+dim(mergeCbgs)
+# Follow the example code to create a variable named FullFips.
+mergeCbgs$FullFips <- mergeCbgs$GEOID10
+# plot(mergeCbgs, lwd=.1, main='Plot of Census Bgs')
+
+# Constructs neighbors from a polygon list.
+neighbor <- poly2nb(mergeCbgs)
+b<-Sys.time()
+
